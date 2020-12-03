c7e6309d969eb739978e41bb82dd55e408e9f4e2
beckettech <71792674+beckettech@users.noreply.github.com>
2020-12-03 01:10:59 -0500

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,8 @@
 #include "OurBST.h"
 #include "Animal.h"
 #include "City.h"
-
+#include "AVL.h"
+#include "chrono"
 using namespace std;
 
 int main() {
@@ -15,26 +16,70 @@ int main() {
 	string state;
 	string strpop;
 	OurBST bst;
+    AVL AVLtree;
 
-	if (myfile.is_open()) {
-		int counter = 0;
+    std::__1::chrono::time_point<std::__1::chrono::steady_clock,
+    std::__1::chrono::duration<long long, std::__1::ratio<1, 1000000000> > > BST_Start_Time;
+
+    std::__1::chrono::time_point<std::__1::chrono::steady_clock,
+    std::__1::chrono::duration<long long, std::__1::ratio<1, 1000000000> > > AVL_Start_Time;
+
+	while(myfile.is_open())
+	{
+	    //start BST timer
+        auto BST_Start = chrono::high_resolution_clock::now();
+        BST_Start_Time = BST_Start;
+	    //insert file in a BST
+		int BSTcounter = 0;
 		getline(myfile,line);
-		while (getline(myfile, name, ',') ) {
+		while (getline(myfile, name, ',') )
+		{
 			getline(myfile, state, ',');
 			getline(myfile, strpop);
-			if (strpop != "A") {
+			if (strpop != "A")
+			{
 				int pop = stoi(strpop);
-				City* city = new City(name, state, pop);
+				City* city = new City(name, pop);
 				bst.insert(city);
-				
-				
 			}
-
-			counter++;
+			BSTcounter++;
 		}
-		cout << "done" << endl;
 		myfile.close();
-		
 	}
-	return 0;
+    //end BST timer
+    auto BST_End = chrono::high_resolution_clock::now();
+    //calculate BST time elapsed
+    auto BST_Time = chrono::duration_cast<chrono::nanoseconds>(BST_End - BST_Start_Time).count();
+    cout<< "BST - Time Elapsed: " << BST_Time <<endl;
+
+    //reopen file for AVL tree
+    ifstream myfile2 ("Project3Data.csv");
+    while(myfile2.is_open())
+    {
+        //start AVL timer
+        auto AVL_Start = chrono::high_resolution_clock::now();
+        AVL_Start_Time = AVL_Start;
+        //insert file in a AVL tree
+        int AVLcounter = 0;
+        getline(myfile, line);
+        while (getline(myfile, name, ','))
+        {
+            getline(myfile, state, ',');
+            getline(myfile, strpop);
+            if (strpop != "A")
+            {
+                int pop = stoi(strpop);
+                City *city = new City(name, pop);
+                AVLtree.insert(city);
+            }
+            AVLcounter++;
+        }
+        myfile2.close();
+    }
+
+    //end BST timer
+    auto AVL_End = chrono::high_resolution_clock::now();
+    //calculate BST time elapsed
+    auto AVL_Time = chrono::duration_cast<chrono::nanoseconds>(AVL_End - AVL_Start_Time).count();
+    cout<< "AVL - Time Elapsed: " << AVL_Time <<endl;
 }
