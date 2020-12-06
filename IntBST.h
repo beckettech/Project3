24f2b60e32d142cb6cc58dc8f528734f4ba52a68
davidvizzi <75280238+davidvizzi@users.noreply.github.com>
2020-12-06 13:56:26 -0600

    Update IntBST.h

--- IntBST.h
+++ IntBST.h
@@ -30,15 +30,15 @@ City* IntBST::FindCity(int pop) {
 
 	while (temp->pop != pop && (temp->right || temp->left)) {
 		if (pop > temp->pop) {
+			highestBelow = temp;
 			if (temp->right) {
-				highestBelow = temp;
 				temp = temp->right;
 			}
 			else {
 				return highestBelow;
 			}
 		}
-		if (pop < temp->pop) {
+		else if (pop < temp->pop) {
 			if (temp->left) {
 				temp = temp->left;
 			}
@@ -46,6 +46,12 @@ City* IntBST::FindCity(int pop) {
 				return highestBelow;
 			}
 		}
+	}
+	if ( !(temp->right || temp->left) ) {
+		if (pop < temp->pop) {
+			return highestBelow;
+		}
+
 	}
 
 	return temp;
