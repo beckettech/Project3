3b2f2e2e4397db2b391f909f3ad021d5418a4e1d
davidvizzi <75280238+davidvizzi@users.noreply.github.com>
2020-12-06 13:23:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,15 +25,15 @@ void readDataName(OurBST& bst, AVL& AVLtree)
 	auto BST_Start_Time = chrono::high_resolution_clock::now();
 	auto AVL_Start_Time = chrono::high_resolution_clock::now();
 
-	while(myfile.is_open())
+	while (myfile.is_open())
 	{
-	    //start BST timer
-        auto BST_Start = chrono::high_resolution_clock::now();
-        BST_Start_Time = BST_Start;
-	    //insert file in a BST
+		//start BST timer
+		auto BST_Start = chrono::high_resolution_clock::now();
+		BST_Start_Time = BST_Start;
+		//insert file in a BST
 		int BSTcounter = 0;
-		getline(myfile,line);
-		while (getline(myfile, name, ',') )
+		getline(myfile, line);
+		while (getline(myfile, name, ','))
 		{
 			getline(myfile, state, ',');
 			getline(myfile, strpop);
@@ -48,42 +48,45 @@ void readDataName(OurBST& bst, AVL& AVLtree)
 		myfile.close();
 		cout << "data read into bst successfully" << endl;
 	}
-    //end BST timer
-    auto BST_End = chrono::high_resolution_clock::now();
-    //calculate BST time elapsed
-    auto BST_Time = chrono::duration_cast<chrono::nanoseconds>(BST_End - BST_Start_Time).count();
+	//end BST timer
+	auto BST_End = chrono::high_resolution_clock::now();
+	//calculate BST time elapsed
+	auto BST_Time = chrono::duration_cast<chrono::nanoseconds>(BST_End - BST_Start_Time).count();
 	cout << "Storing the data in the BST took " << BST_Time << " nanoseconds." << endl;
 
-    //reopen file for AVL tree
-    ifstream myfile2 ("Project3Data.csv");
-    while(myfile2.is_open())
-    {
-        //start AVL timer
-        auto AVL_Start = chrono::high_resolution_clock::now();
-        AVL_Start_Time = AVL_Start;
-        //insert file in a AVL tree
-        int AVLcounter = 0;
-        getline(myfile, line);
-        while (getline(myfile, name, ','))
-        {
-            getline(myfile, state, ',');
-            getline(myfile, strpop);
-            if (strpop != "A")
-            {
-                int pop = stoi(strpop);
-                City *city = new City(name, state, pop);
-                AVLtree.insert(city);
-            }
-            AVLcounter++;
-        }
-        myfile2.close();
+	//reopen file for AVL tree
+	myfile.clear();
+	myfile.seekg(0, myfile.beg);
+	myfile.open("Project3Data.csv");
+	//ifstream myfile ("Project3Data.csv");
+	while (myfile.is_open())
+	{
+		//start AVL timer
+		auto AVL_Start = chrono::high_resolution_clock::now();
+		AVL_Start_Time = AVL_Start;
+		//insert file in a AVL tree
+		int AVLcounter = 0;
+		getline(myfile, line);
+		while (getline(myfile, name, ','))
+		{
+			getline(myfile, state, ',');
+			getline(myfile, strpop);
+			if (strpop != "A")
+			{
+				int pop = stoi(strpop);
+				City* city = new City(name, state, pop);
+				AVLtree.insert(city);
+			}
+			AVLcounter++;
+		}
+		myfile.close();
 		cout << "data read into avl successfully" << endl;
-    }
+	}
 
-    //end AVL timer
-    auto AVL_End = chrono::high_resolution_clock::now();
-    //calculate AVL time elapsed
-    auto AVL_Time = chrono::duration_cast<chrono::nanoseconds>(AVL_End - AVL_Start_Time).count();
+	//end AVL timer
+	auto AVL_End = chrono::high_resolution_clock::now();
+	//calculate AVL time elapsed
+	auto AVL_Time = chrono::duration_cast<chrono::nanoseconds>(AVL_End - AVL_Start_Time).count();
 	cout << "Storing the data in the AVL Tree took " << AVL_Time << " nanoseconds." << endl;
 }
 
@@ -170,39 +173,39 @@ void printAnimalList()
 	cout << "\tDuck:" << endl;
 	cout << "\t\t\t 15 ducks to 1 person" << endl;
 	cout << "\t\t\t 0.067 people to 1 duck" << endl;
-	
+
 	cout << "\tLion:" << endl;
 	cout << "\t\t\t .08 lions to 1 person" << endl;
 	cout << "\t\t\t 12.5 people to 1 lion" << endl;
-	
+
 	cout << "\tT-Rex:" << endl;
 	cout << "\t\t\t .0025 T-Rexes to 1 person" << endl;
 	cout << "\t\t\t 400 people to 1 T-Rex" << endl;
-	
+
 	cout << "\tFire Ant:" << endl;
 	cout << "\t\t\t 750 fire ants to 1 person" << endl;
 	cout << "\t\t\t .0013 people to 1 fire ant" << endl;
-	
+
 	cout << "\tGrizzly Bear:" << endl;
 	cout << "\t\t\t .0625 grizzly bears to 1 person" << endl;
 	cout << "\t\t\t 16 people to 1 grizzly bear" << endl;
-	
+
 	cout << "\tEmu:" << endl;
 	cout << "\t\t\t 1 emu to 1 person" << endl;
 	cout << "\t\t\t 1 person to 1 emu" << endl;
-	
+
 	cout << "\tSquirrel:" << endl;
 	cout << "\t\t\t 22 squirrels to 1 person" << endl;
 	cout << "\t\t\t .046 people to 1 squirrel" << endl;
-	
+
 	cout << "\tChimpanzee:" << endl;
 	cout << "\t\t\t 2 chimpanzees to 1 person" << endl;
 	cout << "\t\t\t .5 people to 1 chimpanzee" << endl;
-	
+
 	cout << "\tGator:" << endl;
 	cout << "\t\t\t .1 gators to 1 person" << endl;
 	cout << "\t\t\t 10 people to 1 gator" << endl;
-	
+
 	cout << "\tGorilla:" << endl;
 	cout << "\t\t\t .05 gorillas to 1 person" << endl;
 	cout << "\t\t\t 20 people to 1 gorilla" << endl;
@@ -224,8 +227,8 @@ void createAnimals(vector<pair<string, float>>& animals)
 
 float getAnimalWeight(string animal, vector<pair<string, float>> animals)
 {
-	for(auto it = animals.begin(); it != animals.end(); ++it){
-		if(it->first == animal){
+	for (auto it = animals.begin(); it != animals.end(); ++it) {
+		if (it->first == animal) {
 			return it->second;
 		}
 	}
@@ -235,10 +238,11 @@ float getAnimalWeight(string animal, vector<pair<string, float>> animals)
 void printCityList(vector<City*> cities)
 {
 	bool firstPrinted = false;
-	for(int i = 0; i < cities.size(); i++){
-		if(firstPrinted == false){
+	for (int i = 0; i < cities.size(); i++) {
+		if (firstPrinted == false) {
 			cout << cities[i]->name;
-		}else{
+		}
+		else {
 			cout << " | " << cities[i]->name;
 		}
 	}
@@ -262,7 +266,7 @@ int main()
 	int userOption = 0;
 	cout << "\nPlease make your selection by entering either \"1\" or \"2\": " << endl;
 	cin >> userOption;
-	cout << "Excellent choice!\n" << endl;	
+	cout << "Excellent choice!\n" << endl;
 
 	// create animal weights
 	vector<pair<string, float>> animals;
@@ -274,13 +278,13 @@ int main()
 	cout << "\n" << endl;
 
 	// actually do what user wants
-	if(userOption == 1){
-		
+	if (userOption == 1) {
+
 		// read in data - sorted by name
 		OurBST bst;
 		AVL avl;
 		readDataName(bst, avl);
-		
+
 		string cityChoice, stateChoice;
 		cout << "Please enter the city to be overthrown: ";
 		cin.ignore();
@@ -292,7 +296,7 @@ int main()
 		cout << "Please select your animal from the above list: ";
 		getline(cin, userAnimal);
 		transform(userAnimal.begin(), userAnimal.end(), userAnimal.begin(), ::tolower);
-		
+
 		// calculate number of animals needed
 		float animalWeight = getAnimalWeight(userAnimal, animals);
 		// run search in BST for cityState
@@ -316,7 +320,7 @@ int main()
 		cout << "Searching through the AVL Tree took " << avlSearchTime << " nanoseconds." << endl;
 
 	}
-	else if(userOption == 2){
+	else if (userOption == 2) {
 
 		// read in data - sorted by population
 		IntBST bstPop;
@@ -328,11 +332,11 @@ int main()
 		vector<pair<string, int>> userAnimalArmy;
 		string userAnimal = "";
 		int numOfAnimal;
-		while(userAnimal != "-1"){
+		while (userAnimal != "-1") {
 			cout << "Please enter the animal name: ";
 			cin.ignore();
 			getline(cin, userAnimal);
-			if(userAnimal == "-1"){
+			if (userAnimal == "-1") {
 				break;
 			}
 			cout << "Please enter the number of " << userAnimal << "s to add: ";
@@ -341,7 +345,7 @@ int main()
 		}
 
 		double maxPop = 0;
-		for(auto it = userAnimalArmy.begin(); it != userAnimalArmy.end(); ++it){
+		for (auto it = userAnimalArmy.begin(); it != userAnimalArmy.end(); ++it) {
 			maxPop += (1 / getAnimalWeight(it->first, animals)) * it->second;
 		}
 		cout << "Your animal army could overthrow any city with a population less than " << maxPop << "!\n" << endl;
@@ -359,13 +363,15 @@ int main()
 
 		// print results
 		cout << "[BST] " << bstCityOverthrown->name << " is the largest city that could be overthrown by your animal army" << endl;
+		cout << "Population: " << bstCityOverthrown->pop << endl;
 		cout << "Searching through the BST took " << bstSearchTime << " nanoseconds." << endl;
 
 		cout << "\n[AVL] " << avlCityOverthrown->name << " is the largest city that could be overthrown by your animal army" << endl;
+		cout << "Population: " << avlCityOverthrown->pop << endl;
 		cout << "Searching through the AVL Tree took " << avlSearchTime << " nanoseconds." << endl;
 
 	}
-	
+
 	cout << "\nThank you for using Lions and Tigers and Bears, Oh My!" << endl;
 	cout << "" << endl;
 
